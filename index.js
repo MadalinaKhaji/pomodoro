ef4f0032abc59082afbcd7293d53b18b54e094fa
Maddie <madalina.khaji10@gmail.com>
2019-10-23 18:48:48 +0200

    implemented start and cancel timer functionality

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}..`);
