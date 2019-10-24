27d3bc39926f861c6dd93af3e72faf23d0fc5d7b
Maddie <madalina.khaji10@gmail.com>
2019-10-24 22:25:58 +0200

    implemented total time and changing the img based on the progress

--- public/functions.js
+++ public/functions.js
@@ -1,31 +1,40 @@
-console.log("hello from the console!");
 
-// 25 min timer 
+let totalTime = 0;
+
 function startTimer() {
-    let seconds = 1500;
+    let seconds = 1500; // 25 minutes
     timerId = setInterval(myTimer, 1000);
 
-    document.getElementById("startButton").className = "hideElement";
-    document.getElementById("cancelButton").className = "";
+    toggleButton("startButton", false);
+    toggleButton("cancelButton", true);
 
     function myTimer() {
         seconds--;
         displayTimer(seconds);
+        if(seconds === 900) { // 15 minutes
+            document.getElementById("tomatoImg").src = "img/yellow-tomato.png";
+        }
+        if(seconds === 300) { // 5 minutes
+            document.getElementById("tomatoImg").src = "img/red-tomato.png";
+        }
         if(seconds === 0) {
             clearInterval(timerId);
             displayTimer(1500);
-            document.getElementById("startButton").className = "";
-            document.getElementById("cancelButton").className = "hideElement";
+            displayTotalTime("0 : 0");
+            toggleButton("startButton", true);
+            toggleButton("cancelButton", false);
             alert("Time is up!");
         }
     }
 }
 
 function cancelTimer() {
+    let currentTime = document.getElementById("timer").innerText;
+    displayTotalTime(currentTime);
     clearInterval(timerId);
     displayTimer(1500);
-    document.getElementById("startButton").className = "";
-    document.getElementById("cancelButton").className = "hideElement";
+    toggleButton("startButton", true);
+    toggleButton("cancelButton", false);
 }
 
 function displayTimer(currentSeconds) {
@@ -34,3 +43,22 @@ function displayTimer(currentSeconds) {
     document.getElementById("timer").innerText = "";
     document.getElementById("timer").innerText = minutes + " : " + seconds; 
 }
+
+function displayTotalTime(theTime) {
+    let min = theTime.trim().slice(0,2);
+
+    totalTime += (25 - min);
+
+    document.getElementById("totalTime").innerText = "";
+    document.getElementById("totalTime").innerText = "Total time: " + totalTime + " minutes";
+}
+
+function toggleButton(buttonId, isDisplayed) {
+    if(isDisplayed === true) {
+        document.getElementById(buttonId).style.display = "initial";
+    } else {
+        document.getElementById(buttonId).style.display = "none";
+    }
+}
+
+toggleButton("cancelButton", false);
